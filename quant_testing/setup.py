76fdb67912bafb211f07a12e64c4ba1028106bbe
Elliot Banks <elliotbanks@bmlltech.com>
2017-08-27 16:35:35 +0100

    Added setup.py

---
+++ quant_testing/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Quant Backtesting Library',
+    version='0.1dev',
+    packages=['quant_testing',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
